58f7b52e5d5e63741f1c7edbca76eea0c4f0608f
target_ <target111@protonmail.com>
2017-12-09 15:54:36 +0200

    Update version.py

--- modules/version.py
+++ modules/version.py
@@ -1,8 +1,8 @@
 import module
 import
 
-@module.commands('version'):
-    def version():
-        version = "Version 3.0"
-        #bot.send(version)
-        print(version)
+@module.commands('version')
+def version():
+    version = "Version 3.0"
+    #bot.send(version)
+    print(version)
